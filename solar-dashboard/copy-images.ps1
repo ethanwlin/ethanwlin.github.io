87ec9483c484361616d9cff3ac7f0ff321617e14
Ethan W. Lin <ethanwlin@gmail.com>
2026-07-22 21:28:38 -0400

    Delete solar-dashboard directory

--- solar-dashboard/copy-images.ps1
+++
@@ -1,7 +0,0 @@
-$dest = "C:\Users\ethan\Projects\solar-dashboard\images"
-New-Item -ItemType Directory -Force -Path $dest | Out-Null
-$src = "C:\Users\ethan\.cursor\projects\empty-window\assets"
-Copy-Item "$src\tesla-powerwall.png" "$dest\tesla-powerwall.png" -Force
-Copy-Item "$src\enphase-iq-battery.png" "$dest\enphase-iq-battery.png" -Force
-Copy-Item "$src\byd-battery-box.png" "$dest\byd-battery-box.png" -Force
-Get-ChildItem $dest | Format-Table Name, Length
